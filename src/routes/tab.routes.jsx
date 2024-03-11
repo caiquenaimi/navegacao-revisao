c704ef8c13ec32c501b766ab370ef7c3fe55def8
caiquenaimi <caiquenaimi.564@gmail.com>
2024-03-11 15:56:39 -0300

    Adiciona ícone na tela inicial

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -1,15 +1,19 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 import Category from "../screens/Category";
+import { Feather } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Home" component={Home} options={
+        tabBarIcon = () => (
+            <Feather name="home" size={24} color="black" />
+        )
+      } />
       <Tab.Screen name="Profile" component={Profile} />
       <Tab.Screen name="Category" component={Category} />
     </Tab.Navigator>
